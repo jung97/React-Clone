e992f5ceef5d3c1796faa7c2037f7f41b3636685
junghyeon <eprtms3@naver.com>
2022-06-15 22:40:28 +0900

    Heder: part 4
    navBar 스크롤 할 때 변화

--- netflixclone/src/Components/Header.tsx
+++ netflixclone/src/Components/Header.tsx
@@ -1,15 +1,14 @@
 import { Link, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
-import { motion } from "framer-motion";
-
-const Nav = styled.nav`
+import { motion, useAnimation, useViewportScroll } from "framer-motion";
+import { useState , useEffect } from 'react';
+const Nav = styled(motion.nav)`
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: fixed;
   width: 100%;
   top: 0;
-  background-color: black;
   font-size: 14px;
   padding: 20px 60px;
   color: white;
@@ -46,13 +45,16 @@ const Item = styled.li`
   }
 `;
 const Search = styled.span`
+  display: flex;
+  align-items: center;
+  position: relative;
   color: white;
   svg {
     height: 25px;
   }
 `;
 
-const Circle = styled.span`
+const Circle = styled(motion.span)`
   position: absolute;
   width: 5px;
   height: 5px;
@@ -76,16 +78,60 @@ const logoVariants = {
   },
 };
 
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  right: 0px;
+  padding: 5px 10px;
+  padding-left: 40px;
+  z-index: -1;
+  color: white;
+  font-size: 16px;
+  background-color: transparent;
+  border: 1px solid ${(props)=> props.theme.white.lighter};
+`;
+
+const navBariants = {
+  top: {
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  scroll: {
+    backgroundColor: "rgba(0,0,0,1)",
+  },
+};
+
 function Header() {
+  const [searchOpen, setSerchOpen] = useState(false);
   const homeMatch = useRouteMatch("/");
   const tvMatch = useRouteMatch("/tv");
+  const inputAnimation = useAnimation();
+  const navAnimation = useAnimation();
+  const {scrollY} =useViewportScroll();
+  const toggleSearch = () => {
+    if(searchOpen) {
+      inputAnimation.start({scaleX:0,});
+    } else {
+      inputAnimation.start({scaleX: 1});
+    }
+    setSerchOpen((prev)=> !prev)
+  };
+  useEffect(()=> {
+    scrollY.onChange(()=>{
+      if (scrollY.get() > 80){
+        navAnimation.start("scroll");
+      } else {
+        navAnimation.start("top");
+      }
+    });
+  },
+  [scrollY,navAnimation]);
   return (
-    <Nav>
+    <Nav variants={navBariants} animate={navAnimation} initial={"top"}>
       <Col>
         <Logo
           variants={logoVariants}
           whileHover="active"
-          initial="normal"
+          animate="normal"
           xmlns="http://www.w3.org/2000/svg"
           width="1024"
           height="276.742"
@@ -95,16 +141,24 @@ function Header() {
         </Logo>
         <Items>
           <Item>
-            <Link to="/">Home {homeMatch?.isExact && <Circle />}</Link>
+            <Link to="/">
+              Home {homeMatch?.isExact && <Circle layoutId="circle" />}
+            </Link>
           </Item>
+          
           <Item>
-            <Link to="/tv">Tv Shows {tvMatch && <Circle />}</Link>
+            <Link to="/tv">
+              Tv Shows {tvMatch && <Circle layoutId="circle" />}
+            </Link>
           </Item>
         </Items>
       </Col>
       <Col>
         <Search>
-          <svg
+          <motion.svg
+            onClick={toggleSearch}
+            animate={{x:searchOpen ? -185: 0}}
+            transition={{type: "linear"}}
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -114,7 +168,13 @@ function Header() {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </motion.svg>
+          <Input
+          transition={{type:"linear"}}
+          animate={inputAnimation}
+          initial={{scaleX:0}}
+          placeholder= "Search for movie or tv show..."
+          />
         </Search>
       </Col>
     </Nav>
